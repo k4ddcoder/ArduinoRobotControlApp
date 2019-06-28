5c4634b482874aceee370d98eb43ebe6b51b3f3c
k4ddcoder <52282447+k4ddcoder@users.noreply.github.com>
2019-06-28 20:42:48 +0200

    Joystick view and other graphical fields added (no movement logic)

--- AndroidApp/RobotControl/app/src/main/java/com/example/robotcontrol/MainActivity.java
+++ AndroidApp/RobotControl/app/src/main/java/com/example/robotcontrol/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.robotcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.jackandphantom.joystickview.JoyStickView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JoyStickView joyStickView = findViewById(R.id.joystick);
+        joyStickView.setOnMoveListener(new JoyStickView.OnMoveListener() {
+            @Override
+            public void onMove(double angle, float strength) {
+
+                System.out.println("Angle: " + angle);
+                System.out.println("Strenght" + strength);
+            }
+        });
     }
+
+
 }
